c76899f594fdefc039e46d9dd9308ac875f991f4
Gioacchino Mazzurco <gmazzurco89@gmail.com>
2010-04-20 00:20:29 +0200

    Revert "fixed syntax error"
    This reverts commit 6075751c8892fcfa9dd7776eff907a3e0d8c70bc.

--- zzzz-eigennet-ipkg-atheros/files/etc/init.d/eigennet_atheros.sh
+++ zzzz-eigennet-ipkg-atheros/files/etc/init.d/eigennet_atheros.sh
@@ -118,7 +118,7 @@ function configureNetwork()
 config interface wifimesh$indi
         option ifname     ath$(($indi*2 + 1))
         option proto      static
-        option ip6addr    '$meshIpV6Subnet:0000:${networkWirelessDevHWAddr6[$indx]}/64'
+        option ip6addr    '$meshIpV6Subnet:0000:${networkWirelessDevHWAddr6[$indx]}'
         option dns        '$meshDns'
 
 config interface wifiap$indi
@@ -163,7 +163,7 @@ Interface \"ath$(($indi*2 + 1))\"
 {
     Mode \"mesh\"
     IPv6Multicast	$OLSRMulticast
-    IPv6Src		$meshIpV6Subnet:0000:${networkWirelessDevHWAddr6[$indx]}
+    Ip6AddrType		global
 }
 "
 
@@ -219,7 +219,7 @@ Interface \"${networkWiredDevice[$indx]}\"
 {
     Mode \"ether\"
     IPv6Multicast	$OLSRMulticast
-    IPv6Src		$meshIpV6Subnet:0000:${networkWiredDevHWAddr6[$indx]}
+    Ip6AddrType         global
 }
 "
 
