42666086413674b08f6ea4c45a7c2d5970572418
arkanet <arkantiko@gmail.com>
2012-10-07 20:27:59 +0300

    added choice for olsrd enable or disable

--- upme.sh
+++ upme.sh
@@ -4,7 +4,7 @@ config_load eigennet
 
 	local cpu=$(cat /proc/cpuinfo |grep type | awk '{ printf "%.4s\n",$5 }')
 	local wget_info=$(opkg info wget | grep Status | awk '{ print $4 }')
-	local ip4_gw_lan		; config_get	ip4_gw_lan		network		"ip4_gw_lan"
+	local ip4_gw_lan	; config_get	ip4_gw_lan	network	"ip4_gw_lan"
 	local gtw=$(ip -4 r s | grep default)
 
 	if [ -n "$gtw" ]
@@ -13,16 +13,16 @@ config_load eigennet
 		else
 			if [ -n "$ip4_gw_lan" ]
 				then
-					echo "The default gateway will be setting: ${ip4_gw_lan}"
+					echo "The default gateway will be set to: ${ip4_gw_lan}"
 					ip -4 r a default via ${ip4_gw_lan} dev br-lan
 					local gtw=$(ip -4 r s | grep default)
-					echo "The default gateway is set correctly"
+					echo "The default gateway is set to: ${gtw}"
 				else
 					echo "Insert the IP of the default gateway and press [ENTER]"
 					read gtw
 					ip -4 r a default via ${gtw} dev br-lan
 					local gtw=$(ip -4 r s | grep default)
-					echo "The default gateway is set correctly"
+					echo "The default gateway is set to: ${gtw}"
 			fi
 	fi
 
@@ -36,11 +36,11 @@ config_load eigennet
 		local path=UBNT_M
 	}
 
-cp /etc/opkg.conf /etc/opkg.bak
-rm -rf /etc/opkg.conf
+mv /etc/opkg.conf /etc/opkg.bak
 touch /etc/opkg.conf
 cat > /etc/opkg.conf << EOF
-src/gz customized_adjustment http://cleopatra.ninux.org/arka_backfire/eigennet-packages-arka/${path}/packages/
+src/gz attitude_adjustment http://downloads.openwrt.org/snapshots/trunk/ar71xx/packages
+src/gz customized_adjustment http://cleopatra.ninux.org/arka_backfire/eigennet-packages-arka/${path}/packages
 dest root /
 dest ram /tmp
 lists_dir ext /var/opkg-lists
@@ -61,7 +61,7 @@ if ping -q -c 3 8.8.8.8 >/dev/null 5>/dev/null; then
 	[ -z $wget_info ] || [ $wget_info = not-installed ] &&
 	{
 		sleep 2s
-		echo wget will be installed
+		echo "wget in not found, so will be installed"
 		sleep 2s
 		opkg install wget
 		sleep 2s
@@ -73,7 +73,31 @@ if ping -q -c 3 8.8.8.8 >/dev/null 5>/dev/null; then
 	wget --no-check-certificate https://raw.github.com/arkanet/eigennet-packages/roma/eigennet/src/eigennet.sh -O /etc/init.d/eigennet
 	chmod +x /etc/init.d/eigennet
 	uci set eigennet.general.bootmode=1
-	uci set eigennet.olsrd.enable=0
+	echo "Do you want enable OLSRd at next reboot ? [1 for enable or 0 for disable] [ENTER]"
+	while read olsrd_enable; do
+		[ $olsrd_enable -eq 1 ] &&
+		{
+			olsrd_enable=$olsrd_enable
+			echo "OLSRd will be enabled at next reboot"
+			uci set eigennet.olsrd.enable=${olsrd_enable}
+			break
+		}
+
+		[ $olsrd_enable -eq 0 ] &&
+		{
+			olsrd_enable=$olsrd_enable
+			echo "OLSRd will be disabled at next reboot"
+			uci set eigennet.olsrd.enable=${olsrd_enable}
+			break
+		}
+
+		[ $enable_disable > 1 ] || [ -z $enable_disable ] &&
+		{
+			echo "$enable_disable is not accepted, try again"
+			echo "Insert 1 for enable or 0 for disable OLSRd at next reboot"
+			continue
+		}
+	done
 	uci commit eigennet
 	sleep 2s
 	echo "Update complete, rebooting in 5 seconds ...."
